201c08ec1dd8ad64cba892dc054ac599e8e60c62
Pirateboyfoxy <146957835+Pirateboyfoxy@users.noreply.github.com>
2025-04-01 12:09:47 -0400

    Challenge

--- Assets/Course Library/Scripts/PlayerController.cs
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour
 {
     public GameObject projectilePrefab;
     float horizontalInput;
+    float verticalInput;
+    public float xRange = 25;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,25 +19,42 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Makes the character move left and right
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * 25 * Time.deltaTime * horizontalInput);
 
-        if(transform.position.x < -20)
+        // Makes the bounds the player can move in
+        if(transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
 
-         if(transform.position.x > 20)
+         if(transform.position.x > xRange)
         {
-            transform.position = new Vector3(10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        // Makes the character move left and right
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * 25 * Time.deltaTime * verticalInput);
+
+        // Makes the bounds the player can move in
+        if(transform.position.z > 28)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 28);
+        }
+
+         if(transform.position.z < -10)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -10);
         }
     
 
-    //check if the player is pressing space
-    if(Input.GetKeyDown(KeyCode.Space))
-    {
-        // launch a projectile from the player
-        Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
-    }
+        //check if the player is pressing space
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            // launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
--- Assets/Course Library/Scripts/SpawnManager.cs
+++ Assets/Course Library/Scripts/SpawnManager.cs
@@ -6,14 +6,18 @@ public class SpawnManager : MonoBehaviour
 {
 
     public GameObject[] animalPrefabs;
+    public GameObject[] animalPrefabsLeft;
+    public GameObject[] animalPrefabsRight;
    private float spawnRangeX = 20;
-   private float spawnRangeZ = 20;
+   private float spawnRangeZ = 30;
    private float startDelay = 2;
    private float spawnInterval = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+        InvokeRepeating("SpawnRandomAnimalLeft", startDelay + 1, spawnInterval);
+        InvokeRepeating("SpawnRandomAnimalRight", startDelay + 1, spawnInterval);
     }
 
     // Update is called once per frame
@@ -27,5 +31,17 @@ public class SpawnManager : MonoBehaviour
         int animalIndex = Random.Range(0, animalPrefabs.Length);
         Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnRangeZ);
         Instantiate(animalPrefabs[animalIndex], spawnPos,animalPrefabs[animalIndex].transform.rotation);
+    }
+    void SpawnRandomAnimalLeft()
+    {
+        int animalIndex = Random.Range(0, animalPrefabsLeft.Length);
+        Vector3 spawnPos = new Vector3(-25, 0, Random.Range(-8, 30));
+        Instantiate(animalPrefabsLeft[animalIndex], spawnPos,animalPrefabsLeft[animalIndex].transform.rotation);
+    }
+    void SpawnRandomAnimalRight()
+    {
+        int animalIndex = Random.Range(0, animalPrefabsRight.Length);
+        Vector3 spawnPos = new Vector3(25, 0, Random.Range(-8, 30));
+        Instantiate(animalPrefabsRight[animalIndex], spawnPos,animalPrefabsRight[animalIndex].transform.rotation);
     }
 }
